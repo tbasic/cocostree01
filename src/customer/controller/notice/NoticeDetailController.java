baccba1573cf2e387e5da2fa5ea7ced030011967
tbasic5 <tbasic@naver.com>
2022-01-07 15:55:53 +0900

    brh s2  detailcontroller

---
+++ src/customer/controller/notice/NoticeDetailController.java
@@ -0,0 +1,17 @@
+package customer.controller.notice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import customer.controller.Controller;
+
+
+
+public class NoticeDetailController implements Controller{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
